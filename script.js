2ed8a2666fe3db560052da6404da0a9f72152c26
Laurent Bosteels <jozefbosteels@MacBook-Pro-van-Jozef.local>
2021-08-03 12:30:10 +0200

    non buggy carousel

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-Array.from(document.querySelectorAll(".letter")).forEach(el => {
-   el.innerText = randomLetter();
+Array.from(document.querySelectorAll(".letter")).forEach((el) => {
+  el.innerText = randomLetter();
 });
 
-function randomLetter(){
-    const alphabet = "abcdefghijklmnopqrstuvwxyz"
-    return alphabet[Math.floor(Math.random() * alphabet.length)]
+function randomLetter() {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  return alphabet[Math.floor(Math.random() * alphabet.length)];
 }
+
+// DARK MODE SWITCH
+function darkMode() {
+  var darkModeSwitch = document.body;
+  darkModeSwitch.classList.toggle("darkMode");
+}
+
+// CAROUSEL
+let i = 0;
+let images = [];
+let time = 3000;
+
+images[0] = "https://placekitten.com/1000/400";
+images[1] = "https://placekitten.com/1100/400";
+images[2] = "https://placekitten.com/1200/400";
+
+function carousel() {
+  document.slide.src = images[i];
+  if (i < images.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
+  setTimeout("carousel()", time);
+}
+
+window.onload = carousel;
